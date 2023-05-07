df514f64baeb6d88071eae27211577e67cb38326
thazin naing <thazinaingmm@gmail.com>
2023-05-07 12:35:08 +0630

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const item1Tag=document.querySelector(".item1")
+const item2Tag=document.querySelector(".item2")
+const item3Tag=document.querySelector(".item3")
+
+// offsetLeft
+console.log(item1Tag.offsetLeft);
+console.log(item2Tag.offsetLeft);
+console.log(item3Tag.offsetLeft);
+
+// offsetWidth
+console.log(item1Tag.offsetWidth);
+console.log(item2Tag.offsetWidth);
+console.log(item3Tag.offsetWidth);
+
